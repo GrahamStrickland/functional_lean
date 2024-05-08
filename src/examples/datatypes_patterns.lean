43a2c72389a95f14199466de99b49c9778f90472
Graham Strickland <grahamhstrickland@gmail.com>
2024-05-08 20:47:24 +0200

    Add rest of dataypes & patterns example

--- src/examples/datatypes_patterns.lean
+++ src/examples/datatypes_patterns.lean
@@ -36,3 +36,42 @@ def depth (p : Point3D) : Float :=
 
 #eval depth ({ x := 0.0, y := -1.0, z := 1.0 } : Point3D)
 
+def even (n : Nat) : Bool :=
+  match n with
+  | Nat.zero => true
+  | Nat.succ k => not (even k)
+
+#eval even 0
+#eval even 5
+#eval even 2
+
+-- def evenLoops (n : Nat) : Bool :=
+--   match n with
+--   | Nat.zero => true
+--   | Nat.succ k => not (evenLoops n)
+
+def plus (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => n
+  | Nat.succ k' => Nat.succ (plus n k')
+
+def times (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => Nat.zero
+  | Nat.succ k' => plus n (times n k')
+
+#eval times 2 3 
+
+def minus (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => n
+  | Nat.succ k' => pred (minus n k')
+
+#eval minus 5 3
+#eval minus 3 5
+
+def div (n : Nat) (k : Nat) : Nat :=
+  if n < k then
+    0
+  else Nat.succ (div (n - k) k)
+
