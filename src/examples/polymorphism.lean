2f6fa9218914f0e6f0d96d01a1aeb4180904ebe0
Graham Strickland <grahamhstrickland@gmail.com>
2024-05-08 21:06:41 +0200

    Add polymorphism example

---
+++ src/examples/polymorphism.lean
@@ -0,0 +1,29 @@
+structure PPoint (α : Type) where
+  x : α
+  y : α
+deriving Repr
+
+def natOrigin : PPoint Nat :=
+  { x := Nat.zero, y := Nat.zero }
+
+def replaceX (α : Type) (point : PPoint α) (newX : α) : PPoint α :=
+  { point with x := newX }
+
+#check (replaceX)
+#check replaceX Nat
+#check replaceX Nat natOrigin
+#check replaceX Nat natOrigin 5
+#eval replaceX Nat natOrigin 5
+
+inductive Sign where 
+  | pos
+  | neg
+
+def posOrNegThree (s : Sign) : match s with | Sign.pos => Nat | Sign.neg => Int :=
+  match s with
+  | Sign.pos => (3 : Nat)
+  | Sign.neg => (-3 : Int)
+
+#check posOrNegThree Sign.pos
+#eval posOrNegThree Sign.pos
+
