7294e41af04bfa526643a001c30bc9aa437bb9d2
Graham Strickland <grahamhstrickland@gmail.com>
2024-06-02 18:48:46 +0200

    Add to Polymorphism examples

--- src/examples/polymorphism.lean
+++ src/examples/polymorphism.lean
@@ -15,7 +15,7 @@ def replaceX (α : Type) (point : PPoint α) (newX : α) : PPoint α :=
 #check replaceX Nat natOrigin 5
 #eval replaceX Nat natOrigin 5
 
-inductive Sign where 
+inductive Sign where
   | pos
   | neg
 
@@ -27,3 +27,60 @@ def posOrNegThree (s : Sign) : match s with | Sign.pos => Nat | Sign.neg => Int 
 #check posOrNegThree Sign.pos
 #eval posOrNegThree Sign.pos
 
+def primesUnder10 : List Nat := [2, 3, 5, 7]
+
+-- inductive List (α : Type) where
+--   | nil : List α
+--   | cons : α → List α → List α
+
+def explicitPrimesUnder10 : List Nat :=
+  List.cons 2 (List.cons 3 (List.cons 5 (List.cons 7 List.nil)))
+
+-- def length (α : Type) (xs : List α) : Nat :=
+--   match xs with
+--   | List.nil => Nat.zero
+--   | List.cons y ys => Nat.succ (length α ys)
+
+-- def length (α : Type) (xs : List α) : Nat :=
+--   match xs with
+--   | [] => 0
+--   | y :: ys => Nat.succ (length α ys)
+
+-- def replaceX {α : Type} (point : PPoint α) (newX : α) : PPoint α :=
+--   { point with x := newX }
+
+-- #eval replaceX natOrigin 5
+
+def length {α : Type} (xs : List α) : Nat :=
+  match xs with
+  | [] => 0
+  | y :: ys => Nat.succ (length ys)
+
+#eval length primesUnder10
+#eval primesUnder10.length
+
+#check List.length (α := Int)
+
+-- inductive Option (α : Type) : Type where
+--   | none : Option α
+--   | some (val : α) : Option α
+
+-- def List.head? {α : Type} (xs : List α) : Option α :=
+--   match xs with
+--   | [] => none
+--   | y :: _ => some y
+
+#eval primesUnder10.head?
+-- #eval [].head?
+#eval [].head? (α := Int)
+#eval ([] : List Int).head?
+
+-- structure Prod (α : Type) (β : Type) : Type where
+--   fst : α
+--   snd : β
+
+-- def fives : String × Int := { fst := "five", snd := 5 }
+def fives : String × Int := ("five", 5)
+
+-- def sevens : String × Int × Nat := ("VII", 7, 4 + 3)
+def sevens : String × (Int × Nat) := ("VII", (7, 4 + 3))
