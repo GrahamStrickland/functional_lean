b0d41fc09b2a8a06121a6ba1e653e846953424e8
Graham Strickland <grahamhstrickland@gmail.com>
2024-10-05 10:01:33 +0200

    Add separate exercises directory

--- src/examples/expressions.lean
+++ src/examples/expressions.lean
@@ -5,8 +5,3 @@
 #eval String.append "great " (String.append "oak " "tree")
 
 #eval String.append "it is " (if 1 > 2 then "yes" else "no")
-
-#eval 42 + 19
-#eval String.append "A" (String.append "B" "C")
-#eval if 3 == 3 then 5 else 7
-#eval if 3 == 4 then "equal" else "not equal"
--- src/examples/structures.lean
+++ src/examples/structures.lean
@@ -68,34 +68,3 @@ def Point.modifyBoth (f : Float → Float) (p : Point) : Point :=
   { x := f p.x, y := f p.y }
 
 #eval fourAndThree.modifyBoth Float.floor
-
-structure RectangularPrism where
-  height : Float
-  width : Float
-  depth : Float
-deriving Repr
-
-def volume (p : RectangularPrism) : Float :=
-  p.height * p.width * p.depth
-
-#eval volume { height := 1.2, width := 2.1, depth := 3.0 }
-
-structure Segment where
-  p1 : Point
-  p2 : Point
-deriving Repr
-
-def length (s : Segment) : Float :=
-  distance s.p2 s.p1
-
-#eval length { p1 := { x := 1.0, y := -1.0}, p2 := { x := 2.0, y := 3.0 } }
-
-structure Hamster where
-  name : String
-  fluffy : Bool
-
-structure Book where
-  makeBook ::
-  title : String
-  author : String
-  price : Float
