486f2855e4f4afbb580ad89514fa3a96306bcf7d
Graham Strickland <grahamhstrickland@gmail.com>
2024-10-20 14:30:22 +0200

    Add List.lastEntry? function

--- src/ch01/exercises/polymorphism.lean
+++ src/ch01/exercises/polymorphism.lean
@@ -1,4 +1,6 @@
-def lastEntry (α : Type) (xs : List α) : Option α :=
-  match xs with
-  | [] => none
-  | y :: _ => some y
+def List.lastEntry? (α : Type) (xs : List α) : Option α := xs[xs.length - 1]?
+
+#eval [1, 2, 3].lastEntry?
+#eval [].lastEntry? (α := Nat)
+#eval [1.4, 2.78, 3.14].lastEntry?
+#eval [].lastEntry? (α := Int)
