e0ab5f5bc0009f71322422f039ddcc7eef8de83f
Graham Strickland <grahamhstrickland@gmail.com>
2024-10-23 21:07:20 +0200

    Add PetName custom datatype exercise

--- src/ch01/exercises/polymorphism.lean
+++ src/ch01/exercises/polymorphism.lean
@@ -22,3 +22,18 @@ def Prod.swap {α β : Type} (pair : α × β) : β × α := (pair.snd, pair.fst
 #eval (1, 2).swap
 #eval ("dog", "cat").swap
 #eval (1.34, 2.56).swap
+
+inductive PetName where
+  | dog (name : String) : PetName
+  | cat (name : String) : PetName
+
+def animals : List PetName :=
+  [PetName.dog "Spot", PetName.cat "Tiger", PetName.dog "Fifi", PetName.dog "Rex", PetName.cat "Floof"]
+
+def howManyDogs (pets : List PetName) : Nat :=
+  match pets with
+  | [] => 0
+  | PetName.dog _ :: morePets => howManyDogs morePets + 1
+  | PetName.cat _ :: morePets => howManyDogs morePets
+
+#eval howManyDogs animals
