e854337f066c7f651e09eea73c40cde55fbcfd40
Graham Strickland <grahamhstrickland@gmail.com>
2024-11-09 20:32:19 +0200

    Add more proposition/proof examples

--- src/ch03/examples/propositions/Main.lean
+++ src/ch03/examples/propositions/Main.lean
@@ -22,5 +22,23 @@ theorem trueIsTrue : True := True.intro
 theorem trueOrFalse : True ∨ False := by simp
 theorem falseImpliesTrue : False → True := by simp
 
+
+def third (xs : List α) (ok : xs.length > 2) : α := xs[2]
+
+def woodlandCritters : List String :=
+  ["hedgehog", "deer", "snail"]
+
+-- #eval third woodlandCritters (by simp)
+
+def thirdOption (xs : List α) : Option α := xs[2]?
+
+#eval thirdOption woodlandCritters
+#eval thirdOption ["only", "two"]
+#eval woodlandCritters[1]!
+
+-- def unsafeThird (xs : List α) : α := xs[2]!
+
+-- #eval woodlandCritters [1]
+
 def main : IO Unit :=
   pure ()
