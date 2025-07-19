18a3ea72aee6e887b2c810d225cf9bb48b2f3623
Graham Strickland <grahamhstrickland@gmail.com>
2025-07-19 17:24:15 +0200

    Add more Positive Numbers examples to Ch03

--- FunctionalLean/Ch03/Ch03/PositiveNumbers.lean
+++ FunctionalLean/Ch03/Ch03/PositiveNumbers.lean
@@ -38,3 +38,81 @@ instance : Add Pos where
 def fourteen : Pos := seven + seven
 
 #eval fourteen + fourteen
+
+def posToString (atTop : Bool) (p : Pos) : String :=
+  let paren s := if atTop then s else "(" ++ s ++ ")"
+  match p with
+  | Pos.one => "Pos.one"
+  | Pos.succ n => paren s!"Pos.succ {posToString false n}"
+
+instance : ToString Pos where
+  toString := posToString true
+
+#eval s!"There are {seven}"
+
+def Pos.toNat : Pos → Nat
+  | Pos.one => 1
+  | Pos.succ n => n.toNat + 1
+
+instance : ToString Pos where
+  toString x := toString (x.toNat)
+
+#eval s!"There are {seven}"
+
+def Pos.mul : Pos → Pos → Pos
+  | Pos.one, k => k
+  | Pos.succ n, k => n.mul k + k
+
+instance : Mul Pos where
+  mul := Pos.mul
+
+#eval [seven * Pos.one,
+       seven * seven,
+       Pos.succ Pos.one * seven]
+
+-- class Zero (α : Type) where
+--   zero : α
+
+class One (α : Type) where
+  one : α
+
+instance : One Pos where
+  one := Pos.one
+
+-- #eval (1 : Pos)
+
+-- class OfNat (α : Type) (_ : Nat) where
+--   ofNat : α
+
+inductive LT4 where
+  | zero
+  | one
+  | two
+  | three
+deriving Repr
+
+instance : OfNat LT4 0 where
+  ofNat := LT4.zero
+
+instance : OfNat LT4 1 where
+  ofNat := LT4.one
+
+instance : OfNat LT4 2 where
+  ofNat := LT4.two
+
+instance : OfNat LT4 3 where
+  ofNat := LT4.three
+
+#eval (3 : LT4)
+#eval (0 : LT4)
+-- #eval (4 : LT4)
+
+instance : OfNat Pos (n + 1) where
+  ofNat :=
+    let rec natPlusOne : Nat → Pos
+      | 0 => Pos.one
+      | k + 1 => Pos.succ (natPlusOne k)
+    natPlusOne n
+
+def eight : Pos := 8
+-- def zero : Pos := 0
