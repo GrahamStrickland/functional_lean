b6c45f7c153f3aeaf21a0c0b081af852e068c18d
Graham Strickland <grahamhstrickland@gmail.com>
2026-03-21 15:16:51 +0200

    Add GetElem examples to ArraysAndIndexing and fix name

--- FunctionalLean/Ch03/Ch03/ArraysAndIndexing.lean
+++ FunctionalLean/Ch03/Ch03/ArraysAndIndexing.lean
@@ -1,3 +1,5 @@
+import Ch03.ControllingInstanceSearch
+
 def northernTrees : Array String :=
   #["sloe", "birch", "elm", "oak"]
 
@@ -40,3 +42,24 @@ def NonEmptyList.get (xs : NonEmptyList α)
   match i with
   | 0 => xs.head
   | n + 1 => xs.tail[n]
+
+-- class GetElem
+--     (coll : Type)
+--     (idx : Type)
+--     (item : outParam Type)
+--     (inBounds : outParam (coll → idx → Prop)) where
+--   getElem : (c : coll) → (i : idx) → inBounds c i → item
+
+instance : GetElem (NonEmptyList α) Nat α NonEmptyList.inBounds where
+  getElem := NonEmptyList.get
+
+#eval idahoSpiders.head
+-- #eval idahoSpiders[9]
+
+instance : GetElem (List α) Pos α
+    (fun list n => list.length > n.toNat) where
+  getElem (xs : List α) (i : Pos) ok := xs[i.toNat]
+
+instance : GetElem (PPoint α) Bool α (fun _ _ => True) where
+  getElem (p : PPoint α) (i : Bool) _ :=
+    if not i then p.x else p.y
