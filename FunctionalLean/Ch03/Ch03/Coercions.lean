7c8f9d7616fe9e818274356e8d02cc7dfaf498ff
Graham Strickland <grahamhstrickland@gmail.com>
2026-05-11 18:54:50 +0200

    Add Coercions chapter

---
+++ FunctionalLean/Ch03/Ch03/Coercions.lean
@@ -0,0 +1,160 @@
+import Ch03.ArraysAndIndexing
+
+def fileDumper : IO Unit := do
+  let stdin ← IO.getStdin
+  let stdout ← IO.getStdout
+  stdout.putStr "Which file? "
+  stdout.flush
+  let f := (← stdin.getLine).trim
+  stdout.putStrLn s!"'The file {f}' contains:"
+  stdout.putStrLn (← IO.FS.readFile f)
+
+-- #eval [1, 2, 3, 4].drop (2 : Pos)
+
+-- class Coe (α : Type) (β : Type) where
+--   coe : α → β
+
+instance : Coe Pos Nat where
+  coe x := x.toNat
+
+#eval [1, 2, 3, 4].drop (2 : Pos)
+
+#check [1, 2, 3, 4].drop (2 : Pos)
+
+def oneInt : Int := Pos.one
+
+#eval oneInt
+
+inductive A where
+  | a
+
+inductive B where
+  | b
+
+instance : Coe A B where
+  coe _ := B.b
+
+instance : Coe B A where
+  coe _ := A.a
+
+instance : Coe Unit A where
+  coe _ := A.a
+
+def coercedToB : B := ()
+
+#eval coercedToB
+
+def List.last? : List α → Option α
+  | [] => none
+  | [x] => x
+  | _ :: x :: xs => last? (x :: xs)
+
+#eval [1, 2, 3, 4].last?
+#eval ([] : (List Nat)).last?
+#eval [1].last?
+
+def perhapsPerhapsPerhaps : Option (Option (Option String)) :=
+  "Please don't tell me"
+
+#eval perhapsPerhapsPerhaps
+
+def perhapsPerhapsPerhapsNat : Option (Option (Option Nat)) :=
+  ↑(392 : Nat)
+
+#eval perhapsPerhapsPerhapsNat
+
+instance : Coe (NonEmptyList α) (List α) where
+  coe
+    | { head := x, tail := xs } => x :: xs
+
+#eval ↑(idahoSpiders: List String).last?
+
+-- class CoeDep (α : Type) (x : α) (β : Type) where
+--   coe : β
+
+instance : CoeDep (List α) (x :: xs) (NonEmptyList α) where
+  coe := { head := x, tail := xs }
+
+structure Monoid where
+  Carrier : Type
+  neutral : Carrier
+  op : Carrier → Carrier → Carrier
+
+def natMulMonoid : Monoid :=
+  { Carrier := Nat, neutral := 1, op := (· * ·) }
+
+def natAddMonoid : Monoid :=
+  { Carrier := Nat, neutral := 0, op := (· + ·) }
+
+def stringMonoid : Monoid :=
+  { Carrier := String, neutral := "", op := String.append }
+
+def listMonoid (α : Type) : Monoid :=
+  { Carrier := List α, neutral := [], op := List.append }
+
+-- def foldMap (M : Monoid) (f : α → M.Carrier) (xs : List α) : M.Carrier :=
+--   let rec go (soFar : M.Carrier) : List α → M.Carrier
+--     | [] => soFar
+--     | y :: ys => go (M.op soFar (f y)) ys
+--   go M.neutral xs
+
+instance : CoeSort Monoid Type where
+  coe m := m.Carrier
+
+def foldMap (M : Monoid) (f : α → M) (xs : List α) : M :=
+  let rec go (soFar : M) : List α → M
+    | [] => soFar
+    | y :: ys => go (M.op soFar (f y)) ys
+  go M.neutral xs
+
+#eval foldMap natAddMonoid id ([1, 2, 3, 4] : List Nat)
+#eval foldMap natMulMonoid id ([1, 2, 3, 4] : List Nat)
+
+instance : CoeSort Bool Prop where
+  coe b := b = true
+
+-- class CoeFun (α : Type) (makeFunctionType : outParam (α → Type)) where
+--   coe : (x : α) → makeFunctionType x
+
+structure Adder where
+  howMuch : Nat
+
+def add5 : Adder := ⟨5⟩
+
+-- #eval add5 3
+
+instance : CoeFun Adder (fun _ => Nat → Nat) where
+  coe a := (· + a.howMuch)
+
+#eval add5 3
+
+inductive JSON where
+  | true : JSON
+  | false : JSON
+  | null : JSON
+  | string : String → JSON
+  | number : Float → JSON
+  | object : List (String × JSON) → JSON
+  | array : List JSON → JSON
+
+structure Serializer where
+  Contents : Type
+  serialize : Contents → JSON
+
+def Str : Serializer :=
+  { Contents := String,
+    serialize := JSON.string
+  }
+
+instance : CoeFun Serializer (fun s => s.Contents → JSON) where
+  coe s := s.serialize
+
+def buildResponse (title : String) (R : Serializer)
+    (record: R.Contents) : JSON :=
+  JSON.object [
+    ("title", JSON.string title),
+    ("status", JSON.number 200),
+    ("record", R record)
+  ]
+
+#eval buildResponse "Functional Programming in Lean" Str "Programming is fun!"
