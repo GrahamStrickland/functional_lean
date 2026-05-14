e3f04dd38409f11864df1f023a9c4c949cb1abd7
Graham Strickland <grahamhstrickland@gmail.com>
2026-05-14 17:37:28 +0200

    Add Additional Conveniences to Ch03

---
+++ FunctionalLean/Ch03/Ch03/AdditionalConveniences.lean
@@ -0,0 +1,37 @@
+import Ch03.ArraysAndIndexing
+
+structure Tree : Type where
+  latinName : String
+  commonNames : List String
+
+def oak : Tree :=
+  ⟨"Quercus robur", ["common oak", "European oak"]⟩
+
+def birch : Tree :=
+  { latinName := "Betula pendula",
+    commonNames := ["silver birch", "warty birch"]
+  }
+
+def sloe : Tree where
+  latinName := "Prunus spinosa"
+  commonNames := ["sloe", "blackthorn"]
+
+class Display (α : Type) where
+  displayName : α → String
+
+instance : Display Tree :=
+  ⟨Tree.latinName⟩
+
+instance : Display Tree :=
+  { displayName := Tree.latinName }
+
+instance : Display Tree where
+  displayName t := t.latinName
+
+example : NonEmptyList String :=
+  { head := "Sparrow",
+    tail := ["Duck", "Swan", "Magpie", "Eurasian coot", "Crow"]
+  }
+
+example (n : Nat) (k : Nat) : Bool :=
+  n + k == k + n
